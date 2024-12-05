875f7d4c9725c2761dd2877c39aafe67c350636b
bublinak <jirkasvihla@seznam.cz>
2024-12-05 20:06:01 +0100

    repair of wrong file katex test 12

--- docsify-katex.js
+++ docsify-katex.js
@@ -1,55 +1,85 @@
-( () => {
-    var e, r;
-    e = window.$docsify.markdown,
-    r = marked,
-    window.$docsify.markdown = function(n, t) {
-        var i, o = {
-            name: "math",
-            level: "inline",
-            start: function(e) {
-                var r;
-                return null === (r = e.match(/\$/)) || void 0 === r ? void 0 : r.index
-            },
-            tokenizer: function(e, r) {
-                var n;
-                return (n = /^\$\$((\\.|[^\$\\])+)\$\$/.exec(e)) ? {
-                    type: "math",
-                    raw: n[0],
-                    text: n[1].trim(),
-                    mathLevel: "block"
-                } : (n = /^\$((\\.|[^\$\\])+)\$/.exec(e)) ? {
-                    type: "math",
-                    raw: n[0],
-                    text: n[1].trim(),
-                    mathLevel: "inline"
-                } : void 0
-            },
-            renderer: function(e) {
-                return "block" === e.mathLevel ? katex.renderToString(e.text, {
-                    throwOnError: !1,
-                    displayMode: !0
-                }) : "inline" === e.mathLevel ? katex.renderToString(e.text, {
-                    throwOnError: !1,
-                    displayMode: !1
-                }) : void 0
+(function () {
+    let oldMarkdown = window.$docsify.markdown;
+    let newMarked = marked.parse; // version above 2.1.0
+    function newMarkdown(originMarked, originRenderer) {
+      // in docsify.js: `window.marked = marked;`
+      // this will overwrite the marked
+      // here `let newMarked = marked;` will not right
+      function isFn(obj) {
+        return typeof obj === "function";
+      }
+      const mathExtension = {
+        name: "math",
+        level: "inline",
+        start(src) {
+          let index = src.match(/\$/)?.index;
+          return index;
+        },
+        tokenizer(src, tokens) {
+          const blockRule = /^\$\$((\\.|[^\$\\])+)\$\$/;
+          const inlineRule = /^\$((\\.|[^\$\\])+)\$/;
+          let match;
+          if ((match = blockRule.exec(src))) {
+            return {
+              type: "math",
+              raw: match[0],
+              text: match[1].trim(),
+              mathLevel: "block",
+            };
+          } else if ((match = inlineRule.exec(src))) {
+            return {
+              type: "math",
+              raw: match[0],
+              text: match[1].trim(),
+              mathLevel: "inline",
+            };
+          }
+        },
+        renderer(token) {
+          if (token.mathLevel === "block") {
+            return katex.renderToString(token.text, {
+              throwOnError: false,
+              displayMode: true,
+            });
+          } else if (token.mathLevel === "inline") {
+            return katex.renderToString(token.text, {
+              throwOnError: false,
+              displayMode: false,
+            });
+          }
+        },
+      };
+      const merge =
+        Object.assign ||
+        function (to) {
+          for (let i = 1; i < arguments.length; i++) {
+            const from = Object(arguments[i]);
+            for (const key in from) {
+              if (hasOwn.call(from, key)) {
+                to[key] = from[key];
+              }
             }
-        }, a = Object.assign || function(e) {
-            for (var r = 1; r < arguments.length; r++) {
-                var n = Object(arguments[r]);
-                for (var t in n)
-                    hasOwn.call(n, t) && (e[t] = n[t])
-            }
-            return e
-        }
-        ;
-        return i = "function" == typeof e ? originMarkdown.apply(this, n, t).defaults : a(e, {
-            renderer: a(t, e.renderer)
-        }),
-        r.setOptions(i),
-        r.use({
-            extensions: [o]
-        }),
-        r
+          }
+          return to;
+        };
+  
+      let opts = oldMarkdown || {};
+  
+      if (isFn(oldMarkdown)) {
+        opts = originMarkdown.apply(
+          this, // make it right: return this.origin.code(src);
+          originMarked,
+          originRenderer
+        ).defaults;
+      } else {
+        opts = merge(opts, {
+          renderer: merge(originRenderer, opts.renderer),
+        });
+      }
+      newMarked.setOptions(opts);
+      newMarked.use({ extensions: [mathExtension] });
+  
+      return newMarked;
     }
-}
-)();
+    window.$docsify.markdown = newMarkdown;
+  })();
